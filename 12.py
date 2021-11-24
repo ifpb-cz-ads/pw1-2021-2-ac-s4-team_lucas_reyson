d8a93d41e0510a7108ac1f30a7741df239ee0d20
Reyson <73318309+nameNosyer@users.noreply.github.com>
2021-11-24 19:34:55 -0300

    Adicionando questão 12

---
+++ 12.py
@@ -0,0 +1,4 @@
+dist = float(input('Infome a distancia em Km: '))
+vel_media = float(input('Informe a distancia media: '))
+tempo = dist / vel_media
+print("O tempo aproxiamdo é de {} horas".format(tempo))
