8f7560655f0223df306c9666bef4911b8e7cbd8c
Reyson <73318309+nameNosyer@users.noreply.github.com>
2021-11-24 19:32:46 -0300

    Adicionando questão 06

---
+++ 06.py
@@ -0,0 +1,7 @@
+materia01 = int (input('Qual sua nota da materia01? '))
+materia02 = int (input('Qual sua nota da materia02? ')) 
+materia03 = int (input('Qual sua nota da materia03? '))
+if materia01 and materia02 and materia03 >= 7:
+    print('O aluno foi aprovado')
+else:
+    print('O aluno não foi aprovado')
