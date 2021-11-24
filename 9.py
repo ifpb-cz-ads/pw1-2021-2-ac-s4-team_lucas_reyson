37eeb7b54398287433c174061344fd22308a10c9
Lucas <76121589+Lucasotaro@users.noreply.github.com>
2021-11-24 19:34:02 -0300

    Adicionando questão 09

---
+++ 9.py
@@ -0,0 +1,6 @@
+dias = int(input('Dias: '))
+horas = int(input('Horas: '))
+minutos = int(input('Minutos: '))
+segundos = int(input('Segundos: '))
+total = dias * 86400 + horas * 3600 + minutos * 60
+print(total)
